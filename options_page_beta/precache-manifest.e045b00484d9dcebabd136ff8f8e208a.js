b6b38de4d095a20ad64e5fe2403914d7d9a39830
Dhruv Techapps <dhruv.techapps@gmail.com>
2020-03-12 17:33:06 +0530

    added static path again to test

---
+++ options_page_beta/precache-manifest.e045b00484d9dcebabd136ff8f8e208a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8733dd8a95cebdd1f606adaaa85b95",
+    "url": "https://getdataentry.com/options_page_beta/index.html"
+  },
+  {
+    "revision": "85dd01c9ccc573167ece",
+    "url": "https://getdataentry.com/options_page_beta/static/css/main.9c39e9cc.chunk.css"
+  },
+  {
+    "revision": "d90689728f157b586988",
+    "url": "https://getdataentry.com/options_page_beta/static/js/2.df329942.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "https://getdataentry.com/options_page_beta/static/js/2.df329942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85dd01c9ccc573167ece",
+    "url": "https://getdataentry.com/options_page_beta/static/js/main.d5c83c4d.chunk.js"
+  },
+  {
+    "revision": "cb39446d969980ef8554",
+    "url": "https://getdataentry.com/options_page_beta/static/js/runtime-main.ab7f9d9b.js"
+  },
+  {
+    "revision": "f542bc97fa98399f16a1fac3a7a08265",
+    "url": "https://getdataentry.com/options_page_beta/static/media/bars.f542bc97.svg"
+  },
+  {
+    "revision": "9c1691a21b58348795be33e7eb701963",
+    "url": "https://getdataentry.com/options_page_beta/static/media/code.9c1691a2.svg"
+  },
+  {
+    "revision": "95fbf42e8c9c2688df0a648d4310fd50",
+    "url": "https://getdataentry.com/options_page_beta/static/media/cog.95fbf42e.svg"
+  },
+  {
+    "revision": "306b4ca7d727addeeb16ed32d743978a",
+    "url": "https://getdataentry.com/options_page_beta/static/media/minus-circle.306b4ca7.svg"
+  },
+  {
+    "revision": "df9f4c9d4a02e3ef029c52165dd29d30",
+    "url": "https://getdataentry.com/options_page_beta/static/media/plus-circle.df9f4c9d.svg"
+  }
+]);
--- options_page_beta/service-worker.js
+++ options_page_beta/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.890f14b27624b9ccf8936ca7bd340125.js"
+  "https://getdataentry.com/options_page_beta/precache-manifest.e045b00484d9dcebabd136ff8f8e208a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://getdataentry.com/options_page_beta/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
